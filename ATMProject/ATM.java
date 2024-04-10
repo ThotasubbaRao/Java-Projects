84e86ba7454f90ec214b690db942dc17e51a1a0c
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-04-10 09:07:11 +0530

    Add files via upload

---
+++ ATMProject/ATM.java
@@ -0,0 +1,12 @@
+package ATMProject;
+import java.io.*;
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args)throws IOException {
+		
+        OptionMenu optionmenu = new OptionMenu();
+        
+        optionmenu.getLogin();
+	}
+
+}
